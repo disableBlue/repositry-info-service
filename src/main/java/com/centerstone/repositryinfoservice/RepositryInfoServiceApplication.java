db806e245ba796228e4e64af847df7816c49abe2
Harsh Rohila <hrohila@eu.trimblecorp.net>
2018-02-08 11:06:43 +0530

    initial commit

---
+++ src/main/java/com/centerstone/repositryinfoservice/RepositryInfoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.centerstone.repositryinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositryInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositryInfoServiceApplication.class, args);
+	}
+}
